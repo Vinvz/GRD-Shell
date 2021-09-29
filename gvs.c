cdf77b3af1cfbaaf32fd107c1051bc87cdace741
Gabriel Vasconcelos <65234592+GabrielDCV@users.noreply.github.com>
2021-09-29 18:52:21 -0300

    GVShell update.

---
+++ gvs.c
@@ -0,0 +1,149 @@
+// C Program to design a shell in Linux
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/wait.h>
+#include<readline/readline.h>
+#include<readline/history.h>
+  
+#define MAXCOM 1000 // max number of letters to be supported
+#define MAXLIST 100 // max number of commands to be supported
+  
+// Function to take input
+int takeInput(char* str)
+{
+    char* buf;
+  
+    buf = readline("\n>>> ");
+    if (strlen(buf) != 0) {
+        add_history(buf);
+        strcpy(str, buf);
+        return 0;
+    } else {
+        return 1;
+    }
+}
+  
+// Function where the system command is executed
+void execArgs(char** parsed)
+{
+    // Forking a child
+    pid_t pid = fork(); 
+  
+    if (pid == -1) {
+        printf("\nFailed forking child..");
+        return;
+    } else if (pid == 0) {
+        if (execvp(parsed[0], parsed) < 0) {
+            printf("\nCould not execute command..");
+        }
+        exit(0);
+    } else {
+        // waiting for child to terminate
+        wait(NULL); 
+        return;
+    }
+}
+  
+// Help command builtin
+void openHelp()
+{
+    puts("help, desenvolvendo.");
+    return;
+}
+  
+// Function to execute builtin commands
+int ownCmdHandler(char** parsed)
+{
+    int NoOfOwnCmds = 4, i, switchOwnArg = 0;
+    char* ListOfOwnCmds[NoOfOwnCmds];
+    char* username;
+    ListOfOwnCmds[0] = "exit";
+    ListOfOwnCmds[1] = "cd";
+    ListOfOwnCmds[2] = "help";
+    ListOfOwnCmds[3] = "hello";
+  
+    for (i = 0; i < NoOfOwnCmds; i++) {
+        if (strcmp(parsed[0], ListOfOwnCmds[i]) == 0) {
+            switchOwnArg = i + 1;
+            break;
+        }
+    }
+  
+    switch (switchOwnArg) {
+    case 1:
+        printf("\nGoodbye\n");
+        exit(0);
+    case 2:
+        chdir(parsed[1]);
+        return 1;
+    case 3:
+        openHelp();
+        return 1;
+    case 4:
+        username = getenv("USER");
+        printf("\nHello %s.\n",
+            username);
+        return 1;
+    default:
+        break;
+    }
+    return 0;
+}
+  
+// function for parsing command words
+void parseSpace(char* str, char** parsed)
+{
+    int i;
+    for (i = 0; i < MAXLIST; i++) {
+        parsed[i] = strsep(&str, " ");
+        if (parsed[i] == NULL)
+            break;
+        if (strlen(parsed[i]) == 0)
+            i--;
+    }
+}
+  
+int processString(char* str, char** parsed)
+{
+    char* strpiped[2];
+    int piped = 0;
+  
+    if (piped) {
+        parseSpace(strpiped[0], parsed);
+  
+    } else {
+        parseSpace(str, parsed);
+    }
+  
+    if (ownCmdHandler(parsed))
+        return 0;
+    else
+        return 1 + piped;
+}
+  
+int main()
+{
+    char inputString[MAXCOM], *parsedArgs[MAXLIST];
+    char* parsedArgsPiped[MAXLIST];
+    int execFlag = 0;
+  
+    while (1) {
+        // take input
+        if (takeInput(inputString))
+            continue;
+        // process
+        execFlag = processString(inputString,
+        parsedArgs);
+        // execflag returns zero if there is no command
+        // or it is a builtin command,
+        // 1 if it is a simple command
+        // 2 if it is including a pipe.
+
+        // execute
+        if (execFlag == 1)
+            execArgs(parsedArgs);
+    }
+    return 0;
+}
