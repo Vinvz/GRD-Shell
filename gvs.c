633fb4c9206ab0ff78a05496326b99a646889d9e
GabrielDCV <gcv@cesar.school>
2021-10-01 14:43:17 -0300

    Shell update with Pipes Function

--- gvs.c
+++ gvs.c
@@ -38,7 +38,61 @@ void execute(char** parsed)
         return;
     }
 }
+
+void execArgsPiped(char** parsed, char** parsedpipe)
+{
+    // 0 is read end, 1 is write end
+    int pipefd[2]; 
+    pid_t p1, p2;
   
+    if (pipe(pipefd) < 0) {
+        printf("\nPipe could not be initialized");
+        return;
+    }
+    p1 = fork();
+    if (p1 < 0) {
+        printf("\nCould not fork");
+        return;
+    }
+  
+    if (p1 == 0) {
+        // Child 1 executing..
+        // It only needs to write at the write end
+        close(pipefd[0]);
+        dup2(pipefd[1], STDOUT_FILENO);
+        close(pipefd[1]);
+  
+        if (execvp(parsed[0], parsed) < 0) {
+            printf("\nCould not execute command 1..");
+            exit(0);
+        }
+    } else {
+        // Parent executing
+        p2 = fork();
+  
+        if (p2 < 0) {
+            printf("\nCould not fork");
+            return;
+        }
+  
+        // Child 2 executing..
+        // It only needs to read at the read end
+        if (p2 == 0) {
+            close(pipefd[1]);
+            dup2(pipefd[0], STDIN_FILENO);
+            close(pipefd[0]);
+            if (execvp(parsedpipe[0], parsedpipe) < 0) {
+                printf("\nCould not execute command 2..");
+                exit(0);
+            }
+        } else {
+            // parent executing, waiting for two children
+            wait(NULL);
+            wait(NULL);
+        }
+    }
+}
+
 // comando built-in de ajuda
 void openHelp()
 {
@@ -85,7 +139,24 @@ int builtin_handler(char** parsed)
     }
     return 0;
 }
+
+// function for finding pipe
+int parsePipe(char* str, char** strpiped)
+{
+    int i;
+    for (i = 0; i < 2; i++) {
+        strpiped[i] = strsep(&str, "|");
+        if (strpiped[i] == NULL)
+            break;
+    }
   
+    if (strpiped[1] == NULL)
+        return 0; // returns zero if no pipe is found.
+    else {
+        return 1;
+    }
+}
+
 // function for parsing command words
 void parse_comms(char* str, char** parsed)
 {
@@ -103,13 +174,14 @@ void parse_comms(char* str, char** parsed)
     }
 }
   
-int process_string(char* str, char** parsed)
+int process_string(char* str, char** parsed, char **parsedpipe)
 {
     char* piped_string[2];
     int piped = 0;
   
     if (piped){
         parse_comms(piped_string[0], parsed);
+        parse_comms(piped_string[1], parsedpipe);
   
     } else {
         parse_comms(str, parsed);
@@ -131,11 +203,13 @@ int main(void)
     while (1){
         if (read_input(input))
             continue;
-        flag = process_string(input, args);
+        flag = process_string(input, args, piped_args);
         // a flag retornará 0 se não houver comando ou se for built-in
         // retornará 1 se for um comando simples
         if (flag == 1)
             execute(args);
+        if (flag == 2)
+            execArgsPiped(args, piped_args);
     }
     return 0;
 }
